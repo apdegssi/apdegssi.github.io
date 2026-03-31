962537d4680ae5b08e2cc37f4cfcbbf4cc6ac16d
Eris Runa <eris.runa@gmail.com>
2026-03-31 11:52:20 +0200

    updating school

--- seminars.py
+++ seminars.py
@@ -13,6 +13,59 @@ CSV_FILE = 'seminars.csv'
 OUTPUT_DIR = 'content/seminars'
 
 
+def get_schedule(time: str, place: str, zoom_link: str):
+
+    TIME = time
+    PLACE = place
+    ZOOMLink = zoom_link
+    # {{< collapsible_button  
+    #     title="Schedule" 
+    #     text=`
+    #     <table style="margin-left: auto; margin-right: auto;>
+    #   <thead>
+    #     <tr style="text-align: right;">
+    #       <th>Lecture</th>
+    #       <th>Time</th>
+    #       <th>Place</th>
+    #       <th>ZoomLink</th>
+    #     </tr>
+    #   </thead>
+    #   <tbody>
+    #     <tr>
+    #       <td>Lecture 1</td>
+    #       <td>10:45-12:15, 28.04.2025</td>
+    #       <td><a href='https://www.google.com/maps/dir//Gran+Sasso+Science+Institute,+Viale+Francesco+Crispi,+7+Rectorate,+Via+Michele+Iacobucci,+2,+67100+L'Aquila+AQ,+Italy/@42.3445687,13.31408'>Main Lecture Hall, Ex-Isef</a></td>
+    #       <td></td>
+    #     </tr>
+    #     <tr>
+    #       <td>Lecture 2</td>
+    #       <td>10:45-12:15, 29.04.2025</td>
+    #       <td><a href='https://www.google.com/maps/dir//Gran+Sasso+Science+Institute,+Viale+Francesco+Crispi,+7+Rectorate,+Via+Michele+Iacobucci,+2,+67100+L'Aquila+AQ,+Italy/@42.3445687,13.31408'>Main Lecture Hall, Ex-Isef</a></td>
+    #       <td></td>
+    #     </tr>
+    #     <tr>
+    #       <td>Lecture 3</td>
+    #       <td>14:15-15:45, 29.04.2025</td>
+    #       <td><a href='https://www.google.com/maps/dir//Gran+Sasso+Science+Institute,+Viale+Francesco+Crispi,+7+Rectorate,+Via+Michele+Iacobucci,+2,+67100+L'Aquila+AQ,+Italy/@42.3445687,13.31408'>Main Lecture Hall, Ex-Isef</a></td>
+    #       <td></td>
+    #     </tr>
+    #     <tr>
+    #       <td>Lecture 4</td>
+    #       <td>16:15-17:45, 30.04.2025</td>
+    #       <td><a href='https://www.google.com/maps/dir//Gran+Sasso+Science+Institute,+Viale+Francesco+Crispi,+7+Rectorate,+Via+Michele+Iacobucci,+2,+67100+L'Aquila+AQ,+Italy/@42.3445687,13.31408'>Main Lecture Hall, Ex-Isef</a></td>
+    #       <td></td>
+    #     </tr>
+    #     <tr>
+    #       <td>Office hours</td>
+    #       <td>14:00-15:00, 30.04.2025</td>
+    #       <td><a href='https://www.google.com/maps/dir//Gran+Sasso+Science+Institute,+Viale+Francesco+Crispi,+7+Rectorate,+Via+Michele+Iacobucci,+2,+67100+L'Aquila+AQ,+Italy/@42.3445687,13.31408'>Main Lecture Hall, Ex-Isef</a></td>
+    #       <td></td>
+    #     </tr>
+    #   </tbody>
+    # </table>`
+    # >}}
+
+
 @lru_cache
 def get_reader_data():
     # Your specific Google Sheet details
